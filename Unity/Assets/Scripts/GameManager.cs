740a67a8f9ef63c2d99b7113aaa60511ba2f6b88
stevo9510 <stevo9510@gmail.com>
2017-03-10 01:02:36 -0500

    More UI Related Changes
    Creating a player menu to show state of player.

---
+++ Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    /// <summary>
+    /// Game Piece Prefab that will be instantiated multiple times at runtime for each player.
+    /// </summary>
+    public GameObject gamePiecePrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
