2d56b6b26b30a7714bc8bfc03a1906620a6f35c3
Clay Baber <claybaber@gmail.com>
2017-04-21 01:58:37 -0700

    Add Network.cs

---
+++ Unity/Assets/Network.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class Network : MonoBehaviour {
+
+	static SocketIOComponent socket;
+
+
+	// Use this for initialization
+	void Start () {
+		socket = GetComponent<SocketIOComponent> ();
+		socket.On ("open", OnConnected);
+	}
+
+
+	void OnConnected(SocketIOEvent obj){
+		Debug.Log ("connected");
+		socket.Emit ("say hi");
+	}
+
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
