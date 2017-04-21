9d507e7ed724be9546911076ca55cc7c2506375e
stevo9510 <stevo9510@gmail.com>
2017-04-21 19:52:26 -0400

    New server to client events publisher interface
    Implementation class is stubbed out for now.

---
+++ Unity/Assets/Scripts/Messages/ServerToClient/StubServerToClientMessagePublisher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// Stub server to client message publisher implementation just to use temporarily until 
+/// actual implementation in Socket.IO class is implemented.
+/// </summary>
+public class StubServerToClientMessagePublisher : IServerToClientMessagePublisher
+{
+    public event Action<AccusationMoveMadeMessage> AccusationMoveMade;
+    public event Action<AccusationResultMessage> AccusationResultReceived;
+    public event Action<CardsDealtMessage> CardsDealt;
+    public event Action<PlayerAssignedIDMessage> PlayerAssignedID;
+    public event Action<PlayerMovedMessage> PlayerMoved;
+    public event Action<PlayersInGameMessage> PlayersInGameChanged;
+    public event Action<PlayerTurnMessage> PlayerTurnChanged;
+    public event Action<SuggestionDebunkMessage> SuggestionDebunkReceived;
+    public event Action<SuggestionMoveMadeMessage> SuggestionMoveMade;
+    public event Action<SuggestionProveOptionMessage> SuggestionProveOptionsReceived;
+    public event Action<SuggestionProveTurnMessage> SuggestionProveTurnChanged;
+    public event Action<WeaponMovedMessage> WeaponMoved;
+}
